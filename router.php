c3c92a587d27409ef79e965940a1f176819fa197
igorperfilspy-droid <igorperfilspy@gmail.com>
2025-08-21 17:11:35 -0300

    Update router.php

--- router.php
+++ router.php
@@ -1,6 +1,6 @@
 <?php
 // Roteador p/ php -S: serve arquivos reais e manda o resto para index.php
-$uri  = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 $path = __DIR__ . $uri;
 
 if ($uri !== '/' && file_exists($path) && !is_dir($path)) {
